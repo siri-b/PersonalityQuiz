5130c988935c25111812bfd6823c46f3c3945827
Siri <Siri@Siris-MacBook-Pro.local>
2019-10-19 17:14:10 -0400

    Add questions

---
+++ Personalityquiz.py
@@ -0,0 +1,68 @@
+color = print(input("Which of these is your favorite color?(green, blue, red or gray): "))
+adjective = print(input("Which adjective do you identify the most with?(realistic, passionate, energetic or outgoing): "))
+work = print(input("When working on a project do you A.Constantly change your plans, B. Make a very organized plan that cannot change, C. start immediatly and not plan or D. Make an organized plan that meets all the expectations but allow yourself to make some changes along the way?: "))
+time = print(input("Do you often find yourself A. losing track of time, B. stressing out about time, C. sticking to a strict time-table, D. using time as a tool: "))
+fun = print(input("When are you the happiest? (A. with family or loved ones, B. when you are comfy in your room, C. When you are at a party with many people or D. When you are having adventures): "))
+
+
+earth = 0
+water = 0
+fire = 0
+air = 0
+
+if color == 'green':
+    earth += 1
+elif color == 'blue':
+    water+= 1
+elif color == 'red':
+    fire += 1
+elif color == 'gray':
+    air +=1
+else:
+    print("Your input is invalid")
+
+if adjective = 'realistic':
+    earth += 1
+elif adjective = 'passionate':
+    water+= 1
+elif adjective = 'outgoing':
+    fire += 1
+elif adjective = 'energetic':
+    air +=1
+else:
+    print("Your input is invalid")
+
+if work = 'B':
+    earth += 1
+elif work = 'D':
+    water+= 1
+elif work = 'C':
+    fire += 1
+elif work = 'A':
+    air +=1
+else:
+    print("Your input is invalid")
+
+if time = 'C':
+    earth += 1
+elif time = 'A':
+    water+= 1
+elif time = 'D':
+    fire += 1
+elif time = 'B':
+    air +=1
+else:
+    print("Your input is invalid")
+
+if time = 'C':
+    earth += 1
+elif time = 'A':
+    water+= 1
+elif time = 'D':
+    fire += 1
+elif time = 'B':
+    air +=1
+else:
+    print("Your input is invalid")
+
+
